2cf857207e61e705cdfec6d77f7e72e3e8dd37e2
Ian Chiu <nk9710632@gmail.com>
2021-12-30 22:09:20 +0800

    feat: use passport local to implement login function

--- services/passport.js
+++ services/passport.js
@@ -35,12 +35,31 @@ module.exports = (passport) => {
         })
         .catch((err) => done(err));
   }));
+
+  /**
+   * Local login strategy.
+   * Authenticate with email address and password.
+   */
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'emailAddress',
+    passwordField: 'password',
+  }, (emailAddress, password, done) => {
+    // check if email address existed
+    User
+        .findOne({where: {emailAddress}})
+        .then((user)=>{
+          // not existed
+          if (!user) {
+            return done(null, false, {message: 'InvalidEmailAddress'});
+          }
+
+          // invalid password
+          if (bcrypt.compareSync(password, user.password)) {
+            return done(null, false, {message: 'InvalidPassword'});
+          }
+
+          return done(null, user);
+        })
+        .catch((err) => done(err));
+  }));
 };
-
-
-// if (!user) {
-//   return done(null, false, {message: 'InvalidEmailAddress'});
-// }
-// if (user.password !== password) {
-//   return done(null, false, {message: 'InvalidPassword'});
-// }
