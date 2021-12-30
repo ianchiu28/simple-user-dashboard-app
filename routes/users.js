b9f6bf84f4141dd7401886f0028c52d8f4f2b67e
Ian Chiu <nk9710632@gmail.com>
2021-12-30 22:11:16 +0800

    feat: add temporary router for login API

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,8 @@ module.exports = (router, passport) => {
   router.post('/signup', passport.authenticate('local-signup'), (req, res) => {
     res.json({a: 'hi'});
   });
+
+  router.post('/login', passport.authenticate('local-login'), (req, res) => {
+    res.json({a: 'ho'});
+  });
 };
