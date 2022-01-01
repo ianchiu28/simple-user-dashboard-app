15a9fb57afc0b9c688f8907394e0cabb3503e536
Ian Chiu <nk9710632@gmail.com>
2022-01-01 14:45:15 +0800

    feat: temporary google login router

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ module.exports = (router, passport) => {
   router.post('/login', passport.authenticate('local-login'), (req, res) => {
     res.json({a: 'ho'});
   });
+
+  router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }));
+
+  router.get(
+      '/auth/google/callback',
+      passport.authenticate('google'),
+      (req, res) => {
+        res.json({a: 'google!'});
+      });
 };
