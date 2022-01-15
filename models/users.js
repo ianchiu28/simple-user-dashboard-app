52cfe1fc8f73564252939f579ed1903d3fd7b65b
Ian Chiu <nk9710632@gmail.com>
2022-01-15 14:56:30 +0800

    docs: add user schema to swagger

--- models/users.js
+++ models/users.js
@@ -1,6 +1,44 @@
 const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
+  /**
+   * @openapi
+   * components:
+   *   schemas:
+   *     User:
+   *       type: object
+   *       properties:
+   *         providerId:
+   *           type: string
+   *           example: test@test.com
+   *         provider:
+   *           type: string
+   *           example: local
+   *         emailAddress:
+   *           type: string
+   *           example: test@test.com
+   *         password:
+   *           type: string
+   *           example: Aa123123!
+   *         username:
+   *           type: string
+   *           example: Test123
+   *         verified:
+   *           type: integer
+   *         verifiedToken:
+   *           type: string
+   *         signUpTimestamp:
+   *           type: string
+   *           example: 2022-01-01T01:02:03.000Z
+   *         loginTimes:
+   *           type: integer
+   *           example: 12
+   *         sessionTimestamp:
+   *           type: string
+   *           example: 2022-01-01T01:02:03.000Z
+   *       required:
+   *         - providerId
+   */
   const User = sequelize.define('User', {
     providerId: {
       type: DataTypes.STRING,
