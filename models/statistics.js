69412f09f26c6fa9c11fa54e5e98144a35b3b743
Ian Chiu <nk9710632@gmail.com>
2022-01-15 19:43:15 +0800

    docs: Statistics schema apidoc

--- models/statistics.js
+++ models/statistics.js
@@ -1,4 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
+  /**
+   * @openapi
+   * components:
+   *   schemas:
+   *     Statistics:
+   *       type: object
+   *       properties:
+   *          date:
+   *            type: string
+   *            example: 2022-01-01 00:00:00+00
+   *          activeUser:
+   *            type: integer
+   *           example: 12
+   *       required:
+   *         - date
+   */
   const Statistic = sequelize.define('Statistic', {
     date: {
       type: DataTypes.DATE,
